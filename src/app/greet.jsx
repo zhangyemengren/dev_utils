98e4a5f49fd35b3774f7746d4d010957c7faf955
zhangyemengren <624593758@qq.com>
2024-02-20 12:07:52 +0800

    feat: add dep

--- src/app/greet.jsx
+++ src/app/greet.jsx
@@ -1,17 +1,26 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react';
-import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import {Button} from '@nextui-org/button';
 
 export default function Greet() {
-    const [greeting, setGreeting] = useState('');
+    const [greeting, setGreeting] = useState("");
 
     useEffect(() => {
-        invoke('greet', { name: 'Next.js' })
-            .then(result => setGreeting(result))
-            .catch(console.error)
-    }, [])
+        invoke("greet", { name: "Next.js" })
+            .then((result) => setGreeting(result))
+            .catch(console.error);
+    }, []);
 
     // Necessary because we will have to use Greet as a component later.
-    return <div>{greeting}</div>;
+    return <div className="text-red-400">
+        {greeting}
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <div>
+            <Button>Click me</Button>
+        </div>
+    </div>;
 }
